8d0a652c4596d8c4f665e3f9fc2fe9f65c9bd26d
Roberto <robertosantana2001@gmail.com>
2021-02-28 00:49:42 -0300

    Adicionou docker-compose versao 2

--- backend/script.sql
+++ backend/script.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE IF NOT EXISTS anunbis;
 use anunbis;
+
+CREATE TABLE IF NOT EXISTS professores(
+codigo int(100) AUTO_INCREMENT,
+nome varchar(30) NOT NULL,
+email varchar(50),
+PRIMARY KEY (codigo)
+);
